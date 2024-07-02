68c47ac6e05f924e342cfa4a09daaab87ce2feea
RaskoProggramer <84768180+RaskoProggramer@users.noreply.github.com>
2024-07-02 09:37:20 +0200

    Delete Mpho directory

--- Mpho/src/main/java/com/enviro/assessment/grad001/mphorakgope/WasteNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.enviro.assessment.grad001.mphorakgope;
-
-public class WasteNotFoundException extends RuntimeException{
-    WasteNotFoundException(Long id){
-        super("Could not find waste "+ id);
-    }
-}
